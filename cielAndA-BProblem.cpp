6dfeee637640a9e9eeeeb0254832922b95001a49
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-13 16:03:40 +0530

    Create cielAndA-BProblem.cpp

---
+++ cielAndA-BProblem.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int a, b;
+    cin >> a >> b;
+    int c = a - b;
+    if (c % 10 == 9) {
+        cout << c - 1 << endl;
+    } else {
+        cout << c + 1 << endl;
+    }
+}
