5024ce394bd4253bd4d6aaaed17731d18efabd61
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-13 16:11:47 +0530

    Create racingHorses.cpp

---
+++ racingHorses.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<long> v(n);
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+        sort(v.begin(), v.end());
+        long ans = INT_MAX;
+        for (int i = 0; i < n-1; i++)
+            ans = min(ans, v[i+1] - v[i]);
+        cout << ans << endl;
+    }
+}
