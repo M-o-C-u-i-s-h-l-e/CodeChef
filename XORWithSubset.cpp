1d264162a75f53ae9b1a1ac027f5328bc427a269
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-28 07:27:01 +0530

    Create XORWithSubset.cpp

---
+++ XORWithSubset.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define N 1007
+
+int main(void) {
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--) {
+    	int n, k, ans = 0;
+    	cin >> n >> k;
+    	vector<int> v(n);
+    	for (int i = 0; i < n; i++)
+    		cin >> v[i];
+    	vector<bool> dp(1024, false);
+    	dp[0] = true;
+    	for (int i = 0; i < n; i++)
+    		for (int j = 0; j < 1024; j++)
+    			dp[j ^ v[i]] = dp[j ^ v[i]] | dp[j];
+    	for (int i = 0; i < 1024; i++)
+    		if (dp[i])
+    			ans = max(ans, k ^ i);
+    	cout << ans << endl;
+    }
+}
