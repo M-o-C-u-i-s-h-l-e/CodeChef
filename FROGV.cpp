6043f836bb68dc743b0e20dc7014f5b821c9dc9c
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-28 10:01:18 +0530

    Create FROGV.cpp

---
+++ FROGV.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+#endif
+
+	int n, k, p;
+	cin >> n >> k >> p;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+	map<int, int, greater<int>> m;
+	for (int i = 0; i < n; i++)
+		m[v[i]] = -1;
+	auto itr = m.begin(), it = m.begin();
+	itr->second = itr->first;
+	for (++itr; itr != m.end(); itr++, it++)
+		itr->second = (it->first - itr->first <= k) ? it->second : itr->first;
+	for (int i = 0, a, b; i < p; i++) {
+		cin >> a >> b;
+		int start = min(v[a-1], v[b-1]), end = max(v[a-1], v[b-1]);
+		(end <= m[start]) ? cout << "Yes\n" : cout << "No\n";
+	}
+}
