a617eff9d6b146789eb1fc6f288dc9d1b74bbc32
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-30 01:38:38 +0530

    Update SCHEDULE.cpp

--- SCHEDULE.cpp
+++ SCHEDULE.cpp
@@ -2,18 +2,11 @@
 using namespace std;
 #define endl "\n"
 
-bool solve(int x, priority_queue<int> pq, int k) {
-	int v = pq.top();
-	while (k > 0 && !pq.empty()) {
-		v = pq.top();
-		pq.pop();
-		if (v > x)
-			k -= (v / (x + 1));
-	}
-	if (pq.empty() && k >= 0)	return true;
-	if (!pq.empty() && pq.top() > x)	return false;
-	if (v / (v / (x + 1) + 1) <= x && k >= 0)	return true;
-	return false;
+bool solve(vector<string> v, int l, int k) {
+	for (string i : v)
+		if (i.size() > l)
+			k -= i.size() / (l + 1);
+	return (k < 0) ? false : true;
 }
 
 int main(void) {
@@ -29,45 +22,43 @@ int main(void) {
 	int t;
 	cin >> t;
 	while (t--) {
-		priority_queue<int> pq;
-		int n, k, Count = 0;
+		int n, k;
 		string str;
 		cin >> n >> k >> str;
-		int Count1 = 0, Count2 = 0;
+		vector<string> v;
+		string prev = "";
+		prev += str[0];
+		for (int i = 1; i < n; i++) {
+			if (str[i] != prev[prev.size()-1])
+				v.push_back(prev), prev = str[i];
+			else
+				prev += str[i];
+		}
+		v.push_back(prev);
+		string s1 = "", s2 = "";
 		for (int i = 0; i < n; i++) {
-			int x = str[i] - '0';
-			(x != i % 2) ? Count1++ : Count2++;
-			if (i == 0) {
-				Count = 1;
-			} else {
-				if (str[i-1] == str[i]) {
-					Count++;
+			s1 += (i%2 == 0) ? '0' : '1';
+			s2 += (i%2 == 0) ? '1' : '0';
+		}
+		int a = 0, b = 0;
+		for (int i = 0; i < n; i++) {
+			if (s1[i] != str[i])	a++;
+			if (s2[i] != str[i])	b++;
+		}
+		if (min(a, b) <= k) {
+			cout << 1 << endl;
+		} else {
+			int l = 2, r = n, res = n;
+			while (l <= r) {
+				int m = l + (r - l) / 2;
+				if (solve(v, m, k)) {
+					res = min(res, m);
+					r = m - 1;
 				} else {
-					pq.push(Count);
-					Count = 1;
+					l = m + 1;
 				}
 			}
+			cout << res << endl;
 		}
-		if (min(Count1, Count2) <= k) {
-			cout << 1 << endl;
-			continue;
-		}
-		pq.push(Count);
-		int ans = pq.top();
-		if (ans == 2 || k == 0) {
-			cout << ans << endl;
-			continue;
-		}
-		int l = max(pq.top() / (k+1) - 1, 2), r = n, res = INT_MAX;
-		while (l <= r) {
-			int m = l + (r - l) / 2;
-			if (solve(m, pq, k)) {
-				res = min(res, m);
-				r = m - 1;
-			} else {
-				l = m + 1;
-			}
-		}
-		cout << res << endl;
 	}
 }
