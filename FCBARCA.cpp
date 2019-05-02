986286573b4e3bd1f268254558ef823b80dec4f1
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-05-02 10:26:30 +0530

    Create FCBARCA.cpp

---
+++ FCBARCA.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+long long mod = 1e9+7;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n, k;
+		cin >> n >> k;
+		long long g[n+1];
+		g[0] = 1, g[1] = 0;
+		for (int i = 2; i <= n; i++)
+			g[i] = ((k-1) * g[i-1] + k * g[i-2]) % mod;
+		cout << g[n] << endl;
+	}
+}
