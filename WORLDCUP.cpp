f517db13105573517bbd15944f1180cb738445d3
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-05-03 12:38:01 +0530

    Update WORLDCUP.cpp

--- WORLDCUP.cpp
+++ WORLDCUP.cpp
@@ -50,3 +50,60 @@ int main(void) {
 		}
 	}
 }
+
+
+
+// Another Solution
+
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+long long mod = 1e9+7;
+long long comb[301][301];
+
+void pre() {
+	memset(comb, 0, sizeof(comb));
+	for (int i = 0; i < 301; i++)
+		comb[i][0] = comb[i][i] = 1;
+	for (int i = 2; i < 301; i++)
+		for (int j = 1; j < i; j++)
+			comb[i][j] = (comb[i-1][j] + comb[i-1][j-1]) % mod;
+}
+
+long long getComb(int a, int b) {
+	return (a < b) ? 0LL : comb[a][b];
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+#endif
+
+	pre();
+	int t;
+	cin >> t;
+	while (t--) {
+		int r, b, l;
+	    cin >> r >> b >> l;
+	    long long sum = 0, six, four;
+	    if(r > b * 6) {
+	        cout << 0 << endl;
+	    } else {
+		    for(int i = 0; i <= r/6; i++) {
+		        six = i;
+		        four = (r - 6 * six) / 4;
+		        if(4 * four + 6 * six == r) {
+			        for(int w = 0; w <= l; w++)
+			            sum = (sum + getComb(b, four) * ((getComb(b-four, six) * getComb(b-four-six, w))%mod))%mod;
+		        }
+		    }
+		    cout << sum << endl;
+		}
+	}
+}
