141e3ee7109365861ad367c220a0045b07c86c69
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-05-13 19:50:37 +0530

    Update SNAKEEAT.py

--- SNAKEEAT.py
+++ SNAKEEAT.py
@@ -1,3 +1,30 @@
+# O (q log n)
+import bisect as bs
+
+for _ in range(int(input())):
+	n, q = map(int, input().split())
+	ls = list(map(int, input().split()))
+	ls.sort()
+	Sum = [0] * n
+	Sum[0] = ls[0]
+	for i in range(1, n):
+		Sum[i] = Sum[i-1] + ls[i]
+	for __ in range(q):
+		k = int(input())
+		pos = bs.bisect_left(ls, k)
+		l, r = 0, pos
+		while l < r:
+			m = l + (r - l) // 2
+			need = k * (pos - m) - (Sum[pos-1] - Sum[m-1])
+			if need <= m:
+				r = m
+			else:
+				l = m + 1
+		print(n - r)
+
+
+
+# O ((n + q) log n)
 for _ in range(int(input())):
 	n, q = map(int, input().split())
 	ls = [0] + list(map(int, input().split()))
