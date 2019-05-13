db704ee8c3b3feddd271f4bf54496b6a22777893
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-05-13 19:43:32 +0530

    Update SNAKEEAT.cpp

--- SNAKEEAT.cpp
+++ SNAKEEAT.cpp
@@ -1,3 +1,53 @@
+// O (q log n)
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cout.precision(6);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n, q;
+		cin >> n >> q;
+		vector<long> v(n);
+		for (int i = 0; i < n; i++)
+			cin >> v[i];
+		sort(v.begin(), v.end());
+		vector<long long> sum(n);
+		sum[0] = v[0];
+		for (int i = 1; i < n; i++)
+			sum[i] = sum[i-1] + v[i];
+		while (q--) {
+			long k;
+			cin >> k;
+			int pos = lower_bound(v.begin(), v.end(), k) - v.begin();
+			int l = 0, r = pos;
+			while (l < r) {
+				int m = l + (r - l) / 2;
+				long long need = k * (pos - m) - (sum[pos-1] - sum[m-1]);
+				if (need <= m)
+					r = m;
+				else
+					l = m + 1;
+			}
+			cout << n - r << endl;
+		}
+	}
+}
+
+
+
+// O ((n + q) * log n)
 #include <bits/stdc++.h>
 using namespace std;
 #define endl "\n"
