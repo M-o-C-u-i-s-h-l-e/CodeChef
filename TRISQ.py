d3c6beb86d17228c55d5aa96e7d27aaf42f09812
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-06-02 01:02:22 +0530

    Update TRISQ.py

--- TRISQ.py
+++ TRISQ.py
@@ -5,3 +5,10 @@ for i in range(4, 10001):
 for _ in range(int(input())):
 	n = int(input())
 	print(ls[n])
+
+	
+	
+# Another Solution
+for _ in range(int(input())):
+	n = int(input()) // 2
+	print((n*n - n) // 2)
