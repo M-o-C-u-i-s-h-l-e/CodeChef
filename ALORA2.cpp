884f6be56273b045263f500560e0e4b75a2a6cd7
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-06-03 20:18:45 +0530

    Update ALORA2.cpp

--- ALORA2.cpp
+++ ALORA2.cpp
@@ -117,3 +117,115 @@ int main(void) {
         }
     }
 }
+
+
+
+
+
+// Another Solution
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+void scanInt(T &x) {
+    bool neg = false;
+    register int c = getchar_unlocked();
+    x = 0;
+    for (; (c < 48 || c > 57) && (c != '-'); c = getchar_unlocked());
+    if (c == '-') {
+        neg = true;
+        c = getchar_unlocked();
+    }
+    for (; c > 47 && c < 58; c = getchar_unlocked())
+        x = (x << 1) + (x << 3) + c - 48;
+    if (neg)
+        x *= -1;
+}
+
+template<typename T>
+void printInt(T n) {
+    if (n < 0){
+        n = -n;
+        putchar_unlocked('-');
+    }
+    int i = 21;
+    char output_buffer[21];
+    do {
+        output_buffer[--i] = (n % 10) + '0';
+        n /= 10;
+    } while (n);
+    do {
+        putchar_unlocked(output_buffer[i]);
+    } while (++i < 21);
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+//  freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    scanInt(t);
+    while (t--) {
+        int n, index1, index;
+        scanInt(n);
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+            scanInt(v[i]);
+        sort(v.begin(), v.end());
+        bool flag = true;
+        for (int i = 2; i < n; i++) {
+            if (v[i] != (v[i-1] + __gcd(v[i-1], v[i-2]))) {
+                flag = false;
+                index1 = v[i];
+                break;
+            }
+        }
+        if (flag) {
+            index = v.back();
+            v.pop_back();
+            v.insert(v.begin(), index);
+            flag = true;
+            for (int i = 2; i < n; i++) {
+                if (v[i] != (v[i-1] + __gcd(v[i-1], v[i-2]))) {
+                    index = v[0];
+                    v.erase(v.begin());
+                    v.emplace_back(index);
+                    break;
+                }
+            }
+        }
+        if (!flag) {
+            for (int i = 0; i < n; i++)
+                if (v[i] == index1) {
+                    v.erase(v.begin() + i);
+                    break;
+                }
+            v.insert(v.begin(), index1);
+            flag = true;
+            for (int i = 2; i < n; i++) {
+                if (v[i] != (v[i-1] + __gcd(v[i-1], v[i-2]))) {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if (n < 3)
+            sort(v.begin(), v.end(), greater<int>());
+        if (!flag) {
+            printInt(-1);
+        } else {
+            for (int i = 0; i < n; i++) {
+                printInt(v[i]);
+                putchar_unlocked(' ');
+            }
+        }
+        putchar_unlocked('\n');
+    }
+}
